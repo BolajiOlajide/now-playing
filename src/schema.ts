604c417e8693c1702bb0263cfcf259671b77425c
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2024-06-11 23:47:02 +0100

    ts init

--- src/schema.ts
+++ src/schema.ts
@@ -1,14 +1,17 @@
-import { z } from 'zod'
+import * as z from 'zod'
 
 export const spotifyProviderSchema = z.object({
   clientId: z.string({
     message: 'Spotify client ID (clientId) is required',
+    required_error: 'Spotify client ID (clientId) is required',
   }),
   clientSecret: z.string({
     message: 'Spotify client secret (clientSecret) is required',
+    required_error: 'Spotify client secret (clientSecret) is required',
   }),
   refreshToken: z.string({
     message: 'Spotify refresh token (refreshToken) is required',
+    required_error: 'Spotify refresh token (refreshToken) is required',
   }),
 })
 

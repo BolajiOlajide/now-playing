01c339a36ec3157325c5d28d4e893b32fc2fab01
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2024-06-12 00:32:00 +0100

    add nvmrc

--- examples/spotify.ts
+++ examples/spotify.ts
@@ -1,7 +1,6 @@
 import { NowPlaying, Providers, StorageKinds } from '../src';
 
-const np = new NowPlaying(
-  Providers.SPOTIFY,
-  StorageKinds.INMEMORY,
-)
+const np = new NowPlaying(Providers.SPOTIFY, {
+  storageKind: StorageKinds.INMEMORY,
+})
 console.log('Hello World', np);

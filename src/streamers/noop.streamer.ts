355262f5638273c6930ba115e3a9407a8ea0c07e
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2024-06-12 01:42:34 +0100

    change song variable

--- src/streamers/noop.streamer.ts
+++ src/streamers/noop.streamer.ts
@@ -1,18 +1,19 @@
-import type { CurrentlyPlaying, IStreamer } from './types'
+import type { Song, IStreamer } from './types'
 
 export class NoopStreamer implements IStreamer {
   public authenticate(): boolean {
     return true
   }
 
-  fetchCurrentlyPlaying(): Promise<CurrentlyPlaying> {
+  fetchCurrentlyPlaying(): Promise<Song> {
     return new Promise((resolve) => {
       resolve({
         title: 'The title',
         artiste: 'The artist',
         image_url: 'https://the.image.url',
-        genre: 'The genre',
+        url: 'https://the.url',
         is_playing: true,
+        preview_url: 'https://the.preview.url',
       })
     })
   }

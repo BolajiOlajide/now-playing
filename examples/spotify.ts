a0e98eb13213db745ff39d1bd47452d11a594edf
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2024-06-24 06:00:07 +0100

    remove reference to StorageKind

--- examples/spotify.ts
+++ examples/spotify.ts
@@ -1,7 +1,6 @@
-import { NowPlaying, Providers, StorageKinds } from "../dist";
+import { NowPlaying, Providers } from "../dist";
 
 const np = new NowPlaying(Providers.SPOTIFY, {
-  storageKind: StorageKinds.INMEMORY,
   streamerArgs: {
     clientId: 'foo',
     clientSecret: 'bar',

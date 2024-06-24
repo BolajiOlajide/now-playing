83b3805d9949d0e942f8afbacf25faa46d46059e
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2024-06-24 05:06:18 +0100

    throw custom error when zod errors

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { ZodError } from 'zod'
+
 import { CACHE_DURATION_MS } from './constants'
 import {
   type StorageKind,
@@ -12,6 +14,7 @@ import {
 } from './schema'
 import { InMemoryStorage, type IStorer } from './storage'
 import { SpotifyStreamer, NoopStreamer, type IStreamer, Song } from './streamers'
+import { ValidationError } from './error'
 
 // NowPlaying allows one to get the currently playing song for a streaming platform
 // user.
@@ -29,9 +32,10 @@ export class NowPlaying {
     provider: Providers,
     args: SpotifyProviderArgs | NoopProviderArgs
   ) {
-    // use zod to guarantee we get the right variable kind in here
-    providerSchema.parse(provider)
-    this.provider = provider
+    try {
+      // use zod to guarantee we get the right variable kind in here
+      providerSchema.parse(provider)
+      this.provider = provider
 
     this.parseArgs(args)
     this.streamerArgs = args.streamerArgs
@@ -42,6 +46,15 @@ export class NowPlaying {
     // this is whatever storage mechanic the user selects
     this.storer = this.getStorer(this.storageKind)
     this.streamer = this.getStreamer()
+    } catch (err: unknown) {
+      if (err instanceof ZodError) {
+        // We want to display Zod errors one at a time, so we stick
+        // to a similar format for returning errors.
+        const [firstError] = err.errors
+        throw new ValidationError(firstError.message)
+      }
+      throw new Error((err as Error)?.message)
+    }
   }
 
   private parseArgs(args: unknown): void {

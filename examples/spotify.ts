55a5924c46678d295aea048ad3f5ee10b2f94b54
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2024-06-26 01:12:03 +0100

    prettify everything

--- examples/spotify.ts
+++ examples/spotify.ts
@@ -1,11 +1,11 @@
 import { NowPlaying, Providers } from '../dist'
 
 const np = new NowPlaying(Providers.SPOTIFY, {
-    streamerArgs: {
-        clientId: 'foo',
-        clientSecret: 'bar',
-        refreshToken: 'baz',
-    },
+  streamerArgs: {
+    clientId: 'foo',
+    clientSecret: 'bar',
+    refreshToken: 'baz',
+  },
 })
 
 console.log('Hello World', np)
